b125c5e2070e7553e2ad2df27c323911a2705a4e
DongGi-create <84391428+DongGi-create@users.noreply.github.com>
2023-07-08 23:57:10 +0900

    백그라운드 스레드에서 돌리도록 수정

--- app/src/main/java/com/recipx/recipx/PX_API/Top_PX_Product.java
+++ app/src/main/java/com/recipx/recipx/PX_API/Top_PX_Product.java
@@ -1,5 +1,6 @@
 package com.recipx.recipx.PX_API;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -34,7 +35,9 @@ public class Top_PX_Product extends AppCompatActivity {
     TextView info;
     PX_Product product;
     ArrayList<ArrayList<PX_Product>> productlist;
-    int year=0; int month=0;
+    int year = 0;
+    int month = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,58 +45,57 @@ public class Top_PX_Product extends AppCompatActivity {
         px_product_recyclerview = findViewById(R.id.px_product_recyclerview);
         standard_btn = findViewById(R.id.standard_btn);
         info = findViewById(R.id.tv_Year_Month);
-        new Thread(new Runnable() {
+
+        new PXProductAsyncTask().execute();
+
+        standard_btn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void run() {
-                // TODO Auto-generated method stub
-                if(productlist==null){
-                    productlist = getPX_Product();
-                    for(int i =0;i<=1;i++){
-                        for(PX_Product P:productlist.get(i)){
-                            // 웹 크롤링하여 이미지 URL 가져오기
-                            if(P.getTitle().contains("테라")||P.getTitle().contains("참이슬")||P.getTitle().contains("카스")){
-                                P.setSrc(P.getTitle());
-                            }
-                            else{
-                                String imageURL = getImageURLFromWeb(P.getTitle());
-                                P.setSrc(imageURL);
-                                Log.d("minseok",P.getTitle()+" "+imageURL);
-                            }
+            public void onClick(View view) {
+                if (standard_btn.getText().equals("금액")) {
+                    standard_btn.setText("수량");
+                    adapter = new PX_Product_Adapter(getApplicationContext(), productlist.get(1));
+                } else {
+                    standard_btn.setText("금액");
+                    adapter = new PX_Product_Adapter(getApplicationContext(), productlist.get(0));
+                }
+                px_product_recyclerview.setAdapter(adapter);
+            }
+        });
+    }
+
+    private class PXProductAsyncTask extends AsyncTask<Void, Void, ArrayList<ArrayList<PX_Product>>> {
+        @Override
+        protected ArrayList<ArrayList<PX_Product>> doInBackground(Void... voids) {
+            if (productlist == null) {
+                productlist = getPX_Product();
+                for (int i = 0; i <= 1; i++) {
+                    for (PX_Product P : productlist.get(i)) {
+                        if (P.getTitle().contains("테라") || P.getTitle().contains("참이슬") || P.getTitle().contains("카스")) {
+                            P.setSrc(P.getTitle());
+                        } else {
+                            String imageURL = getImageURLFromWeb(P.getTitle());
+                            P.setSrc(imageURL);
+                            Log.d("minseok", P.getTitle() + " " + imageURL);
                         }
                     }
                 }
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        // TODO Auto-generated method stub
-                        // 초기에는 "수량" 항목을 보여줌
-                        info.setText(year+"년 "+month+"월");
-                        adapter = new PX_Product_Adapter(getApplicationContext(), productlist.get(0));
-                        px_product_recyclerview.setAdapter(adapter);
-                        standard_btn.setOnClickListener(new View.OnClickListener() {
-                            @Override
-                            public void onClick(View view) {
-                                if(standard_btn.getText().equals("금액")){
-                                    standard_btn.setText("수량");
-                                    adapter = new PX_Product_Adapter(getApplicationContext(),productlist.get(1));
-                                }
-                                else{
-                                    standard_btn.setText("금액");
-                                    adapter = new PX_Product_Adapter(getApplicationContext(),productlist.get(0));
-                                }
-                                px_product_recyclerview.setAdapter(adapter);
-                            }
-                        });
-                        GridLayoutManager gridLayoutManager = new GridLayoutManager(getApplicationContext(),2,GridLayoutManager.VERTICAL,false);
-                        px_product_recyclerview.setLayoutManager(gridLayoutManager);
-                    }
-                });
             }
-        }).start();
+            return productlist;
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<ArrayList<PX_Product>> result) {
+            info.setText(year + "년 " + month + "월");
+            adapter = new PX_Product_Adapter(getApplicationContext(), result.get(0));
+            px_product_recyclerview.setAdapter(adapter);
+            GridLayoutManager gridLayoutManager = new GridLayoutManager(getApplicationContext(), 2, GridLayoutManager.VERTICAL, false);
+            px_product_recyclerview.setLayoutManager(gridLayoutManager);
+        }
     }
-    ArrayList<ArrayList<PX_Product>> getPX_Product(){
-        int maxyear=0, maxmonth=0;
-        ArrayList<ArrayList<PX_Product>> productlist=new ArrayList<>();
+
+    ArrayList<ArrayList<PX_Product>> getPX_Product() {
+        int maxyear = 0, maxmonth = 0;
+        ArrayList<ArrayList<PX_Product>> productlist = new ArrayList<>();
         ArrayList<PX_Product> productlist_cost = new ArrayList<>();
         ArrayList<PX_Product> productlist_cnt = new ArrayList<>();
         String key = BuildConfig.PX_API_KEY;
@@ -102,49 +104,42 @@ public class Top_PX_Product extends AppCompatActivity {
         String startpoint = "0";
         String endpoint = "2500";
 
-        String queryUrl = "https://openapi.mnd.go.kr/"+key+"/"+type+"/"+service+"/"+startpoint+"/"+endpoint;
+        String queryUrl = "https://openapi.mnd.go.kr/" + key + "/" + type + "/" + service + "/" + startpoint + "/" + endpoint;
 
         try {
-            URL url= new URL(queryUrl);//문자열로 된 요청 url을 URL 객체로 생성.
-            InputStream is= url.openStream(); //url위치로 입력스트림 연결
+            URL url = new URL(queryUrl);
+            InputStream is = url.openStream();
 
-            XmlPullParserFactory factory= XmlPullParserFactory.newInstance();
-            XmlPullParser xpp= factory.newPullParser();
-            xpp.setInput( new InputStreamReader(is, "UTF-8") ); //inputstream 으로부터 xml 입력받기
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            XmlPullParser xpp = factory.newPullParser();
+            xpp.setInput(new InputStreamReader(is, "UTF-8"));
 
             String tag;
             xpp.next();
-            int eventType= xpp.getEventType();
+            int eventType = xpp.getEventType();
 
-            while(eventType != XmlPullParser.END_DOCUMENT){
-                switch(eventType){
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                switch (eventType) {
                     case XmlPullParser.START_DOCUMENT:
                         break;
 
                     case XmlPullParser.START_TAG:
-                        tag= xpp.getName();//태그 이름 얻어오기
-                        if(tag.equals("list_total_count")){
-
-                        }
-                        else if(tag.equals("row")) {
+                        tag = xpp.getName();
+                        if (tag.equals("list_total_count")) {
+                        } else if (tag.equals("row")) {
                             product = new PX_Product();
-                        }
-                        else if(tag.equals("rowno")) {
+                        } else if (tag.equals("rowno")) {
                             xpp.next();
-                        }
-                        else if(tag.equals("sellyear")) {
+                        } else if (tag.equals("sellyear")) {
                             xpp.next();
                             product.setYear(xpp.getText());
-                        }
-                        else if(tag.equals("sellmonth")) {
+                        } else if (tag.equals("sellmonth")) {
                             xpp.next();
                             product.setMonth(xpp.getText());
-                        }
-                        else if(tag.equals("seltnstd")) {
+                        } else if (tag.equals("seltnstd")) {
                             xpp.next();
                             product.setStandard(xpp.getText());
-                        }
-                        else if(tag.equals("prdtnm")) {
+                        } else if (tag.equals("prdtnm")) {
                             xpp.next();
                             product.setTitle(xpp.getText());
                             int nowyear = Integer.parseInt(product.getYear());
@@ -154,45 +149,52 @@ public class Top_PX_Product extends AppCompatActivity {
                                 maxyear = nowyear;
                                 maxmonth = nowmonth;
                             }
-                            if(maxyear==nowyear){
-                                if(maxmonth==nowmonth){
-                                    if(standard.equals("수량"))productlist_cnt.add(product);
-                                    else if(standard.equals("금액"))productlist_cost.add(product);
-                                }
-                                else if(maxmonth<nowmonth){
+                            if (maxyear == nowyear) {
+                                if (maxmonth == nowmonth) {
+                                    if (standard.equals("수량"))
+                                        productlist_cnt.add(product);
+                                    else if (standard.equals("금액"))
+                                        productlist_cost.add(product);
+                                } else if (maxmonth < nowmonth) {
                                     maxyear = nowyear;
                                     maxmonth = nowmonth;
                                     productlist_cnt.clear();
                                     productlist_cost.clear();
-                                    if(standard.equals("수량"))productlist_cnt.add(product);
-                                    else if(standard.equals("금액"))productlist_cost.add(product);
+                                    if (standard.equals("수량"))
+                                        productlist_cnt.add(product);
+                                    else if (standard.equals("금액"))
+                                        productlist_cost.add(product);
                                 }
-                            }
-                            else if(maxyear<nowyear) {
+                            } else if (maxyear < nowyear) {
                                 maxyear = nowyear;
                                 maxmonth = nowmonth;
                                 productlist_cnt.clear();
                                 productlist_cost.clear();
-                                if(standard.equals("수량"))productlist_cnt.add(product);
-                                else if(standard.equals("금액"))productlist_cost.add(product);
+                                if (standard.equals("수량"))
+                                    productlist_cnt.add(product);
+                                else if (standard.equals("금액"))
+                                    productlist_cost.add(product);
                             }
                         }
                         break;
+
                     case XmlPullParser.TEXT:
                         break;
 
                     case XmlPullParser.END_TAG:
                         break;
                 }
-                eventType= xpp.next();
+                eventType = xpp.next();
             }
 
         } catch (Exception e) {
-            // TODO Auto-generated catch blocke.printStackTrace();
+            e.printStackTrace();
         }
+
         productlist.add(productlist_cnt);
         productlist.add(productlist_cost);
-        year = maxyear; month = maxmonth;
+        year = maxyear;
+        month = maxmonth;
         return productlist;
     }
 
